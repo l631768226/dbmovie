cdb6c4f33dd2995325db41a250203b2d9ab697b0
ly <631768226@qq.com>
2019-05-09 15:20:21 +0800

    1.完成排名列表基本信息的获取，并在最后一页能正常停止。

--- dbmovie/spiders/dbmovie.py
+++ dbmovie/spiders/dbmovie.py
@@ -9,12 +9,32 @@ class DbmovieSpider(scrapy.Spider):
 
     def parse(self, response):
 
+        base_url = 'https://movie.douban.com/top250'
+
         name = response.xpath('//div[@id="wrapper"]/div[@id="content"]/h1/text()').extract()[0]
         print(name)
 
         movieLi = response.xpath('//*[@id="content"]/div/div[1]/ol/li')
 
         for li in movieLi:
+            rank = li.xpath('./div/div[1]/em/text()').extract()[0]
+            print(rank)
             title = li.xpath('./div/div[2]/div[1]/a/span[1]/text()').extract()[0]
             print(title)
+            score = li.xpath('./div/div[2]/div[2]/div/span[2]/text()').extract()[0]
+            print(score)
+            commentCount = li.xpath('./div/div[2]/div[2]/div/span[4]').extract()[0]
+            print(commentCount)
+            movieLink = li.xpath('./div/div[2]/div[1]/a/@href').extract()[0]
+            print(movieLink)
 
+        try:
+            next_url = response.xpath('//*[@id="content"]/div/div[1]/'
+                                   'div[2]/span[3]/a/@href').extract()[0]
+        except:
+            next_url = None
+        if next_url is not None:
+            next_url = base_url + next_url
+            yield response.follow(next_url, callback = self.parse)
+        else:
+            print("It is over")
